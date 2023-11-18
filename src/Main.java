53fa29611d3fde7a440108407c2db32fc479e934
Giulio Marciante <87944393+undeermind@users.noreply.github.com>
2023-11-18 16:34:05 +0100

    Esercizio HashSet2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+//Scrivere una funzione che restituisca un HashSet riempito
+//Creare un oggetto dello stesso tipo inserito nell'HashSet e popolarlo
+//Verificare che l' elemento sia parte del Set e stampare il risultato
 public class Main {
     public static void main(String[] args) {
+        Number num = new Number(4);
 
+        Set<Integer> numbers = makeHashSet(num);
+
+        if(numbers.contains(4)){
+            System.out.println("The number is contained in the set");
         }
+        else{
+            System.out.println("The number is not contained in the set");
+        }
+    }
+    public static Set<Integer> makeHashSet(Number num){
+        Set<Integer> hashSetNumbers = new HashSet<Integer>(Arrays.asList(1,2,3));
+
+        hashSetNumbers.add(num.number);
+
+        return hashSetNumbers;
+    }
 }
